593ff1bbd25d9b9f5c22d20c8b3424e03a7a598d
DorotaHajduk <dorota.hajduk@interia.pl>
2018-12-13 16:19:22 +0100

    zadanie Admin Benedykt

---
+++ autoJava/src/test/java/selenium/Zadanie_Admin/BenedyktAdminTest.java
@@ -0,0 +1,13 @@
+package selenium.Zadanie_Admin;
+
+import org.junit.jupiter.api.Test;
+
+public class BenedyktAdminTest {
+    public class BenedyktAdminTest extends BaseTest;
+
+    @Test
+    public void LoginAddNoteAndLogout() {
+        String pageUrl = "https://automatyzacja.benedykt.net/wp-admin";
+
+    }
+}
